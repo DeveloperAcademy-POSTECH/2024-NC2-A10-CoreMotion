89f5f9432607cf6c506e62fae122ed5b7babe63f
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-06-15 20:02:34 +0900

    Init

---
+++ 2024-NC2-A10-CoreMotion Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2024-NC2-A10-CoreMotion Watch App
+//
+//  Created by Hyungeol Lee on 6/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2024-NC2-A10-CoreMotion Watch App/_024_NC2_A10_CoreMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  _024_NC2_A10_CoreMotionApp.swift
+//  2024-NC2-A10-CoreMotion Watch App
+//
+//  Created by Hyungeol Lee on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct _024_NC2_A10_CoreMotion_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
